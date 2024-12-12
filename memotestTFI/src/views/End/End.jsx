1f327f821246d50e4f1c4c34a0b65693ab2c81c2
Thiago <sofarelli.thiago10@gmail.com>
2024-12-11 21:31:24 -0300

    Agrego confetti en la View End

--- memotestTFI/src/views/End/End.jsx
+++ memotestTFI/src/views/End/End.jsx
@@ -1,8 +1,11 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import useWindowSize from 'react-use/lib/useWindowSize'
 import './End.css';
+import Confetti from 'react-confetti'
 
 const End = () => {
     const navigate = useNavigate();
+    const { width, height } = useWindowSize()
     const location = useLocation();
     const { score, scorePlayer2, mode } = location.state || { score: 0, scorePlayer2 : 0, mode : undefined };
 
@@ -11,12 +14,20 @@ const End = () => {
 
             {mode === "2" ? (
                 <>
+                    <Confetti
+                        width={width}
+                        height={height}
+                    />
                     <h1>¡Juego finalizado!</h1>
                     <h3>Puntaje Jugador 1: {score}</h3>
                     <h3>Puntaje Jugador 2: {scorePlayer2}</h3>
                 </>
             ) : mode === "1" ? (
                 <>
+                    <Confetti
+                        width={width}
+                        height={height}
+                    />
                     <h1>¡Juego finalizado!</h1>
                     <h3 className={"player-score"}>Obtuviste un puntaje de {score}</h3>
                 </>
